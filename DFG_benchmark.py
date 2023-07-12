21baba3ce28a7db84df7f87d845b8cf0fd8c9dde
James D. Trotter <james@simula.no>
2023-07-12 22:43:14 +0200

    print more info about solvers and number of iterations

--- DFG_benchmark.py
+++ DFG_benchmark.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 import numpy as np
 import ufl
@@ -8,6 +9,8 @@ from mpi4py import MPI
 from petsc4py import PETSc
 
 from create_and_convert_2D_mesh import markers
+
+PROGRAM_VERSION = '1.0.0'
 
 has_tqdm = True
 try:
@@ -18,10 +21,14 @@ except ModuleNotFoundError:
         print("To view progress with progressbar please install tqdm: `pip3 install tqdm`")
 
 def IPCS(comm, xdmffile: str, xdmffacetfile: str, dim: int, degree_u: int,
-         out_u, out_p,
+         out_u, out_p, verbose,
          jit_options: dict = {"cffi_extra_compile_args": ["-Ofast", "-march=native"], "cffi_libraries": ["m"]}):
     assert degree_u >= 2
 
+    if verbose:
+        PETSc.Options()["tent_ksp_view"] = ""
+        PETSc.Options()["corr_ksp_view"] = ""
+        PETSc.Options()["up_ksp_view"] = ""
 
     # Read in mesh
     with io.XDMFFile(comm, xdmffile, "r") as xdmf:
@@ -34,6 +41,51 @@ def IPCS(comm, xdmffile: str, xdmffacetfile: str, dim: int, degree_u: int,
     with io.XDMFFile(comm, xdmffacetfile, "r") as xdmf:
         mt = xdmf.read_meshtags(mesh, "Facet tags")
 
+    mesh.topology.create_entities(2)
+    mesh.topology.create_entities(1)
+    if verbose > 0:
+        num_cells = np.asarray([mesh.topology.index_map(3).size_local], dtype='l')
+        total_cells = np.array(0, dtype='l')
+        comm.Reduce([num_cells, MPI.LONG], [total_cells, MPI.LONG], op=MPI.SUM, root=0)
+        min_cells = np.array(0, dtype='l')
+        comm.Reduce([num_cells, MPI.LONG], [min_cells, MPI.LONG], op=MPI.MIN, root=0)
+        max_cells = np.array(0, dtype='l')
+        comm.Reduce([num_cells, MPI.LONG], [max_cells, MPI.LONG], op=MPI.MAX, root=0)
+        num_faces = np.asarray([mesh.topology.index_map(2).size_local], dtype='l')
+        total_faces = np.array(0, dtype='l')
+        comm.Reduce([num_faces, MPI.LONG], [total_faces, MPI.LONG], op=MPI.SUM, root=0)
+        min_faces = np.array(0, dtype='l')
+        comm.Reduce([num_faces, MPI.LONG], [min_faces, MPI.LONG], op=MPI.MIN, root=0)
+        max_faces = np.array(0, dtype='l')
+        comm.Reduce([num_faces, MPI.LONG], [max_faces, MPI.LONG], op=MPI.MAX, root=0)
+        num_edges = np.asarray([mesh.topology.index_map(1).size_local], dtype='l')
+        total_edges = np.array(0, dtype='l')
+        comm.Reduce([num_edges, MPI.LONG], [total_edges, MPI.LONG], op=MPI.SUM, root=0)
+        min_edges = np.array(0, dtype='l')
+        comm.Reduce([num_edges, MPI.LONG], [min_edges, MPI.LONG], op=MPI.MIN, root=0)
+        max_edges = np.array(0, dtype='l')
+        comm.Reduce([num_edges, MPI.LONG], [max_edges, MPI.LONG], op=MPI.MAX, root=0)
+        num_vertices = np.asarray([mesh.topology.index_map(0).size_local], dtype='l')
+        total_vertices = np.array(0, dtype='l')
+        comm.Reduce([num_vertices, MPI.LONG], [total_vertices, MPI.LONG], op=MPI.SUM, root=0)
+        min_vertices = np.array(0, dtype='l')
+        comm.Reduce([num_vertices, MPI.LONG], [min_vertices, MPI.LONG], op=MPI.MIN, root=0)
+        max_vertices = np.array(0, dtype='l')
+        comm.Reduce([num_vertices, MPI.LONG], [max_vertices, MPI.LONG], op=MPI.MAX, root=0)
+        if comm.rank == 0:
+            print("mesh cells: %ld - per process: %ld to %ld"
+                  % (total_cells, min_cells, max_cells),
+                  file=sys.stderr, flush=True)
+            print("mesh faces: %ld - per process: %ld to %ld"
+                  % (total_faces, min_faces, max_faces),
+                  file=sys.stderr, flush=True)
+            print("mesh edges: %ld - per process: %ld to %ld"
+                  % (total_edges, min_edges, max_edges),
+                  file=sys.stderr, flush=True)
+            print("mesh vertices: %ld - per process: %ld to %ld"
+                  % (total_vertices, min_vertices, max_vertices),
+                  file=sys.stderr, flush=True)
+
     # Create output files
     if out_u:
         out_u.write_mesh(mesh)
@@ -44,10 +96,33 @@ def IPCS(comm, xdmffile: str, xdmffacetfile: str, dim: int, degree_u: int,
     V = fem.VectorFunctionSpace(mesh, ("CG", degree_u))
     Q = fem.FunctionSpace(mesh, ("CG", degree_u - 1))
 
+    if verbose > 0:
+        num_Qdofs = np.asarray([Q.dofmap.index_map.size_local * Q.dofmap.index_map_bs], dtype='l')
+        total_Qdofs = np.array(0, dtype='l')
+        comm.Reduce([num_Qdofs, MPI.LONG], [total_Qdofs, MPI.LONG], op=MPI.SUM, root=0)
+        min_Qdofs = np.array(0, dtype='l')
+        comm.Reduce([num_Qdofs, MPI.LONG], [min_Qdofs, MPI.LONG], op=MPI.MIN, root=0)
+        max_Qdofs = np.array(0, dtype='l')
+        comm.Reduce([num_Qdofs, MPI.LONG], [max_Qdofs, MPI.LONG], op=MPI.MAX, root=0)
+        num_Vdofs = np.asarray([V.dofmap.index_map.size_local * V.dofmap.index_map_bs], dtype='l')
+        total_Vdofs = np.array(0, dtype='l')
+        comm.Reduce([num_Vdofs, MPI.LONG], [total_Vdofs, MPI.LONG], op=MPI.SUM, root=0)
+        min_Vdofs = np.array(0, dtype='l')
+        comm.Reduce([num_Vdofs, MPI.LONG], [min_Vdofs, MPI.LONG], op=MPI.MIN, root=0)
+        max_Vdofs = np.array(0, dtype='l')
+        comm.Reduce([num_Vdofs, MPI.LONG], [max_Vdofs, MPI.LONG], op=MPI.MAX, root=0)
+        if comm.rank == 0:
+            print("pressure degrees-of-freedom: %ld - per process: %ld to %ld"
+                  % (total_Qdofs, min_Qdofs, max_Qdofs),
+                  file=sys.stderr, flush=True)
+            print("velocity degrees-of-freedom: %ld - per process: %ld to %ld"
+                  % (total_Vdofs, min_Vdofs, max_Vdofs),
+                  file=sys.stderr, flush=True)
+
     # Temporal parameters
     t = 0
     dt = PETSc.ScalarType(1e-2)
-    T = 8
+    T = 0.05
 
     # Physical parameters
     nu = 0.001
@@ -141,6 +216,8 @@ def IPCS(comm, xdmffile: str, xdmffacetfile: str, dim: int, degree_u: int,
     solver_tent.rtol = rtol
     solver_tent.setType("bcgs")
     solver_tent.getPC().setType("jacobi")
+    solver_tent.setOptionsPrefix("tent_")
+    solver_tent.setFromOptions()
     # solver_tent.setType("preonly")
     # solver_tent.getPC().setType("lu")
     # solver_tent.getPC().setFactorSolverType("mumps")
@@ -156,6 +233,8 @@ def IPCS(comm, xdmffile: str, xdmffacetfile: str, dim: int, degree_u: int,
     solver_corr.setType("gmres")
     solver_corr.getPC().setType("hypre")
     solver_corr.getPC().setHYPREType("boomeramg")
+    solver_corr.setOptionsPrefix("corr_")
+    solver_corr.setFromOptions()
 
     solver_up = PETSc.KSP().create(comm)
     solver_up.setOperators(A_up)
@@ -167,6 +246,8 @@ def IPCS(comm, xdmffile: str, xdmffacetfile: str, dim: int, degree_u: int,
     solver_up.max_it = 200
     solver_up.setType("cg")
     solver_up.getPC().setType("jacobi")
+    solver_up.setOptionsPrefix("up_")
+    solver_up.setFromOptions()
 
     # Solve problem
     if out_u:
@@ -180,9 +261,12 @@ def IPCS(comm, xdmffile: str, xdmffacetfile: str, dim: int, degree_u: int,
         time_range = range(N)
     for i in time_range:
 
+        if comm.rank == 0:
+            print("Time step %d of %d (t=%g):" % (i+1, N, t))
+
         t += dt
         # Solve step 1
-        with common.Timer("~Step 1"):
+        with common.Timer("~Step 1 (assemble)"):
             u_inlet.interpolate(inlet_velocity(t))
             A_tent.zeroEntries()
             fem.petsc.assemble_matrix(A_tent, a_tent, bcs=bcs_tent)  # type: ignore
@@ -193,8 +277,17 @@ def IPCS(comm, xdmffile: str, xdmffacetfile: str, dim: int, degree_u: int,
             fem.petsc.apply_lifting(b_tent.vector, [a_tent], [bcs_tent])
             b_tent.x.scatter_reverse(la.ScatterMode.add)
             fem.petsc.set_bc(b_tent.vector, bcs_tent)
+            #solver_tent.solve(b_tent.vector, u_tent.vector)
+            #u_tent.x.scatter_forward()
+
+        with common.Timer("~Step 1 (solve)"):
             solver_tent.solve(b_tent.vector, u_tent.vector)
             u_tent.x.scatter_forward()
+
+        if comm.rank == 0:
+            print("Step 1 %s after %d iterations" % (
+                'converged' if solver_tent.converged else (f'diverged (%s)' % solver_tent.reason),
+                solver_tent.its))
 
         # Solve step 2
         with common.Timer("~Step 2"):
@@ -213,6 +306,11 @@ def IPCS(comm, xdmffile: str, xdmffacetfile: str, dim: int, degree_u: int,
             u_old.x.array[:] = uh.x.array
             u_old.x.scatter_forward()
 
+        if comm.rank == 0:
+            print("Step 2 %s after %d iterations" % (
+                'converged' if solver_corr.converged else (f'diverged (%s)' % solver_corr.reason),
+                solver_corr.its))
+
         # Solve step 3
         with common.Timer("~Step 3"):
             b_up.x.array[:] = 0
@@ -221,25 +319,44 @@ def IPCS(comm, xdmffile: str, xdmffacetfile: str, dim: int, degree_u: int,
             solver_up.solve(b_up.vector, uh.vector)
             uh.x.scatter_forward()
 
+        if comm.rank == 0:
+            print("Step 3 %s after %d iterations" % (
+                'converged' if solver_up.converged else (f'diverged (%s)' % solver_up.reason),
+                solver_up.its))
+
         with common.Timer("~IO"):
             if out_u:
                 out_u.write_function(uh, t)
             if out_p:
                 out_p.write_function(ph, t)
 
-    t_step_1 = comm.gather(common.timing("~Step 1"), root=0)
+    t_step_1_assemble = comm.gather(common.timing("~Step 1 (assemble)"), root=0)
+    t_step_1_solve = comm.gather(common.timing("~Step 1 (solve)"), root=0)
     t_step_2 = comm.gather(common.timing("~Step 2"), root=0)
     t_step_3 = comm.gather(common.timing("~Step 3"), root=0)
     io_time = comm.gather(common.timing("~IO"), root=0)
     if comm.rank == 0:
         print("Time-step breakdown")
-        for i, step in enumerate([t_step_1, t_step_2, t_step_3]):
-            step_arr = np.asarray(step)
-            time_per_run = step_arr[:, 1] / step_arr[:, 0]
-            print(f"Step {i+1}: Min time: {np.min(time_per_run):.3e}, Max time: {np.max(time_per_run):.3e}")
+
+        t_step_1_assemble_arr = np.asarray(t_step_1_assemble)
+        time_per_run = t_step_1_assemble_arr[:, 1] / t_step_1_assemble_arr[:, 0]
+        print(f"Step 1 (assemble): Min time: {np.min(time_per_run):.6g} seconds, Max time: {np.max(time_per_run):.6g} seconds")
+
+        t_step_1_solve_arr = np.asarray(t_step_1_solve)
+        time_per_run = t_step_1_solve_arr[:, 1] / t_step_1_solve_arr[:, 0]
+        print(f"Step 1 (solve): Min time: {np.min(time_per_run):.6g} seconds, Max time: {np.max(time_per_run):.6g} seconds")
+
+        t_step_2_arr = np.asarray(t_step_2)
+        time_per_run = t_step_2_arr[:, 1] / t_step_2_arr[:, 0]
+        print(f"Step 2: Min time: {np.min(time_per_run):.6g} seconds, Max time: {np.max(time_per_run):.6g} seconds")
+
+        t_step_3_arr = np.asarray(t_step_3)
+        time_per_run = t_step_3_arr[:, 1] / t_step_3_arr[:, 0]
+        print(f"Step 3: Min time: {np.min(time_per_run):.6g} seconds, Max time: {np.max(time_per_run):.6g} seconds")
+
         io_time_arr = np.asarray(io_time)
         time_per_run = io_time_arr[:, 1] / io_time_arr[:, 0]
-        print(f"IO {i+1}:   Min time: {np.min(time_per_run):.3e}, Max time: {np.max(time_per_run):.3e}")
+        print(f"IO {i+1}:   Min time: {np.min(time_per_run):.6g} seconds, Max time: {np.max(time_per_run):.6g} seconds")
 
     # common.list_timings(comm, [common.TimingType.wall])
 
@@ -256,7 +373,8 @@ if __name__ == "__main__":
     _2D.add_argument('--3D', dest='threed', action='store_true', help="Use 3D mesh", default=False)
     parser.add_argument("--u-output-file", default=None, type=str, dest="u_outfile", help="name of output file for velocity")
     parser.add_argument("--p-output-file", default=None, type=str, dest="p_outfile", help="name of output file for pressure")
-    parser.add_argument("--verbose", default=0, type=int, dest="verbose", help="be more verbose")
+    parser.add_argument("-v", "--verbose", default=0, action='count', dest="verbose", help="be more verbose")
+    parser.add_argument('--version', action='version', version='%(prog)s ' + PROGRAM_VERSION)
     args = parser.parse_args()
     dim = 3 if args.threed else 2
 
@@ -272,7 +390,7 @@ if __name__ == "__main__":
         out_p = io.XDMFFile(comm, args.p_outfile, "w")
 
     IPCS(comm, args.xdmffile, args.xdmffacetfile, dim=dim, degree_u=args.degree,
-         out_u=out_u, out_p=out_p)
+         out_u=out_u, out_p=out_p, verbose=args.verbose)
 
     if out_u:
         out_u.close()
